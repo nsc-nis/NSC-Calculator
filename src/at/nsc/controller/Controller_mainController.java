51c29d2294d289113ef26aa576ef735634f0e3ec
Niklas Schachl <niklas.schachl@htl-wels.at>
2020-12-21 10:12:29 +0100

    New file structure

---
+++ src/at/nsc/controller/Controller_mainController.java
@@ -0,0 +1,10 @@
+package at.nsc.controller;
+
+/**NSC Calculator - MainController
+ * @author Niklas Schachl
+ * @version 1.4, 9.12.2020
+ */
+public class Controller_mainController
+{
+
+}
---
+++ src/at/nsc/main/Main.java
@@ -0,0 +1,22 @@
+package at.nsc.main;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+/**NSC Calculator - Main
+ * @author Niklas Schachl
+ * @version 1.4, 9.12.2020
+ */
+public class Main extends Application
+{
+    public static void main(String[] args)
+    {
+        Application.launch();
+    }
+
+    @Override
+    public void start(Stage stage) throws Exception
+    {
+
+    }
+}
