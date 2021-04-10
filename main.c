1ab581731faa17ca2b31bdf0d3abba9f99a67ec6
Niklas Schachl <niklas.schachl@htl-wels.at>
2021-04-10 14:25:44 +0200

    Compare operations implemented

--- main.c
+++ main.c
@@ -7,6 +7,9 @@
 #define mul "mul"
 #define div "div"
 #define mod "mod"
+#define inc "inc"
+#define dec "dec"
+#define cmp "cmp"
 #define strc "strcmp"
 #define stca "strcat"
 
@@ -21,12 +24,14 @@ float division(float num1, float num2);
 int modulo(float num1, float num2);
 float increment(float num);
 float decrement(float num);
+int compare (float num1, float num2);
 
 //declare variables
 bool isRunning;
 char input[50];
 float input1;
 float input2;
+char output;
 char strinput1[];
 char strinput2[];
 
@@ -39,8 +44,9 @@ int main()
     do
     {
         printMenu();
+        fflush(stdin);
         gets(input);
-        printf("* Your chose: %s\n", input);
+        printf("* You chose: %s\n", input);
 
         if (strcmp(input, add) == 0)
         {
@@ -51,13 +57,74 @@ int main()
         }
         if (strcmp(input, sub) == 0)
         {
-            getNumInput("addition");
+            getNumInput("subtraction");
             printf("*  ___________________________________________________________________________ \n");
-            printf("* | Result of %f + %f : %f\n", input1, input2, addition(input1, input2));
+            printf("* | Result of %f - %f : %f\n", input1, input2, subtraction(input1, input2));
             printf("* |___________________________________________________________________________|\n");
         }
-
-
+        if (strcmp(input, mul) == 0)
+        {
+            getNumInput("multiplication");
+            printf("*  ___________________________________________________________________________ \n");
+            printf("* | Result of %f * %f : %f\n", input1, input2, multiplication(input1, input2));
+            printf("* |___________________________________________________________________________|\n");
+        }
+        if (strcmp(input, div) == 0)
+        {
+            getNumInput("division");
+            printf("*  ___________________________________________________________________________ \n");
+            printf("* | Result of %f / %f : %f\n", input1, input2, division(input1, input2));
+            printf("* |___________________________________________________________________________|\n");
+        }
+        if (strcmp(input, mod) == 0)
+        {
+            getNumInput("modulo");
+            printf("*  ___________________________________________________________________________ \n");
+            printf("* | Result of %f %% %f : %d\n", input1, input2, modulo(input1, input2));
+            printf("* |___________________________________________________________________________|\n");
+        }
+       if (strcmp(input, inc) == 0)
+       {
+           printf("* Please type in a number to increment:\n");
+           printf("./CALCULATOR/increment/");
+           fflush(stdin);
+           scanf("%f", &input1);
+           printf("*  ___________________________________________________________________________ \n");
+           printf("* | Increment of %f : %f\n", input1, increment(input1));
+           printf("* |___________________________________________________________________________|\n");
+       }
+        if (strcmp(input, dec) == 0)
+        {
+            printf("* Please type in a number to decrement:\n");
+            printf("./CALCULATOR/decrement/");
+            fflush(stdin);
+            scanf("%f", &input1);
+            printf("*  ___________________________________________________________________________ \n");
+            printf("* | Decrement of %f : %f\n", input1, decrement(input1));
+            printf("* |___________________________________________________________________________|\n");
+        }
+        if (strcmp(input, cmp) == 0)
+        {
+            getNumInput("compare");
+            switch (compare(input1, input2))
+            {
+                case 1:
+                    output = '<';
+                    break;
+                case 2:
+                    output = '>';
+                    break;
+                case 3:
+                    output = '=';
+                    break;
+                default:
+                    break;
+            }
+            printf("*  ___________________________________________________________________________ \n");
+            printf("* | Result: %f %c %f\n", input1, output ,input2);
+            printf("* |___________________________________________________________________________|\n");
+        }
+        isRunning = false;
     }
     while (isRunning);
 
@@ -69,6 +136,12 @@ int main()
 void printMenu()
 {
     //Main Menu user interface
+    printf("   _____      _            _       _             \n");
+    printf("  / ____|    | |          | |     | |            \n");
+    printf(" | |     __ _| | ___ _   _| | __ _| |_ ___  _ __ \n");
+    printf(" | |    / _` | |/ __| | | | |/ _` | __/ _ \\| '__|\n");
+    printf(" | |___| (_| | | (__| |_| | | (_| | || (_) | |   \n");
+    printf("  \\_____\\__,_|_|\\___|\\__,_|_|\\__,_|\\__\\___/|_|   \n");
     printf("*  ___________________________________________________________________________ \n");
     printf("* |                   Welcome to NSC Calculator 1.5!                          |\n");
     printf("* |                         Development Edition                               |\n");
@@ -78,13 +151,14 @@ void printMenu()
     printf("* | subtraction - (sub)                                                       |\n");
     printf("* | multiplication * (mul)                                                    |\n");
     printf("* | division / (div)                                                          |\n");
-    printf("* | modulo %% (mod)                                                           |\n");
+    printf("* | modulo %% (mod)                                                            |\n");
     printf("* | String compare (strcmp)                                                   |\n");
     printf("* | String link (strcat)                                                      |\n");
     printf("* | increment ++ (inc)                                                        |\n");
     printf("* | decrement -- (dec)                                                        |\n");
+    printf("* | compare ==, <, > (cmp)                                                    |\n");
     printf("* |___________________________________________________________________________|\n");
-    printf("* | Type \"exit\" to quit the program                                         |\n");
+    printf("* | Type \"exit\" to quit the program                                           |\n");
     printf("* |___________________________________________________________________________|\n");
     printf("./CALCULATOR/");
 }
@@ -92,27 +166,27 @@ void printMenu()
 void getNumInput(char operator[])
 {
     printf("* Please type in the first number for the calculation:\n");
-    printf("./CALCULATOR/%s", operator);
-    scanf("%f", input1);
+    printf("./CALCULATOR/%s/", operator);
+    fflush(stdin);
+    scanf("%f", &input1);
 
     printf("* Please type in the second number for the calculation:\n");
-    printf("./CALCULATOR/%s", operator);
-    scanf("%f", input2);
-
-    printf("* Read numbers: %f %f", input1, input2);
+    printf("./CALCULATOR/%s/", operator);
+    fflush(stdin);
+    scanf("%f", &input2);
 }
 
 void getStringInput(char operator[])
 {
     printf("* Please type in the first string for the calculation:\n");
-    printf("./CALCULATOR/%s", operator);
-    scanf("%s", strinput1);
+    printf("./CALCULATOR/%s/", operator);
+    fflush(stdin);
+    gets(strinput1);
 
     printf("* Please type in the second string for the calculation:\n");
-    printf("./CALCULATOR/%s", operator);
-    scanf("%s", strinput2);
-
-    printf("* Read numbers: %f %f", input1, input2);
+    printf("./CALCULATOR/%s/", operator);
+    fflush(stdin);
+    gets(strinput2);
 }
 
 float addition(float num1, float num2)
@@ -144,10 +218,52 @@ int modulo(float num1, float num2)
 
 float increment(float num)
 {
-    return num++;
+    return ++num;
 }
 
 float decrement(float num)
 {
-    return num--;
+    return --num;
+}
+
+int compare (float num1, float num2)
+{
+    /*
+     * Wieso funktioniert das hier nicht?
+     * Wieso returned ein char-funktion einen Integer-Wert??
+
+    int ir1 = (int)num1;
+    int ir2 = (int)num2;
+    switch (ir1)
+    {
+        case ir1 < ir2:
+            return "<";
+            break;
+        case num1 > num2:
+            return ">";
+            break;
+        case num1 == num2:
+            return "==";
+            break;
+        default:
+            printf("*  _______\n");
+            printf("* | ERROR | An unexpected error occurred\n");
+            printf("* |_______|\n");
+            return "!";
+            break;
+    }
+     */
+    if (num1 < num2)
+        return 1;
+    else if (num1 > num2)
+        return 2;
+    else if (num1 == num2)
+        return 3;
+    else
+    {
+        printf("*  _______\n");
+        printf("* | ERROR | An unexpected error occurred\n");
+        printf("* |_______|\n");
+        return -1;
+    }
 }
